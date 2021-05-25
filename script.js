9d5bd1822d25eb8336c02856c8c5107b62178327
amalga80 <mohamed.saied7378@gmail.com>
2021-05-25 16:26:33 +0200

    adding new game feature

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // 2. Check if player's score  us >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       // Finish the game
       playing = false;
       document
@@ -90,7 +90,7 @@ btnHold.addEventListener('click', function () {
 });
 
 // Rest the game
-btnNew.addEventListener('click', initt);
+btnNew.addEventListener('click', init);
 
 const newFeature = function () {
   console.log('Welcome to the application');
